b6d4f4c524a78144a183cc31a988fb9f7e465bf9
popso <car8298@gmail.com>
2019-04-09 17:51:34 +0900

    dddd

---
+++ JavaStudy/src/ch05/Exam5_3.java
@@ -0,0 +1,18 @@
+package ch05;
+
+public class Exam5_3 {
+	public static void main(String[] args) {
+		char[] nums = { 'a', 'b', 'c', 'd', 'e', 'f', 'g' };
+		for (int i = 0; i < nums.length; i++) {
+			char temp = ' ';
+			int random = (int)(Math.random() * nums.length);
+			temp = nums[0];
+			nums[0] = nums[random];
+			nums[random] = temp;
+			
+			// 코드 작성
+		}
+
+		System.out.println(nums);
+	}
+}
---
+++ JavaStudy/src/ch05/HomeWork.java
@@ -0,0 +1,24 @@
+package ch05;
+
+public class HomeWork {
+	public static void main(String[] args) {
+		// 배열 입력
+		int[] stick = { 5, 4, 4, 2, 2, 8};
+		// 최소값, 횟수 변수 선언
+		int min = 0;
+		int count = 0;
+		// 최소값 찾기
+		for(int i = 0; i < stick.length; i++) {
+			if(stick[i] < min || min == 0) {
+			}	min = stick[i];
+			} // 각 막대기에서 짧은 막대기 만큼 자르기
+			for(int i = 0; i < stick.length; i++) {
+				stick[i] = stick[i] - min;
+				if(i >= 0)
+				
+				count++;
+				System.out.println(stick[i]);
+		}
+	System.out.println(count);	
+	}
+}
