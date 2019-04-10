179680a1f573d62bb95625edaef7687548ee4649
popso <car8298@gmail.com>
2019-04-10 09:36:05 +0900

    stick_solution

--- JavaStudy/src/ch05/HomeWork.java
+++ JavaStudy/src/ch05/HomeWork.java
@@ -6,7 +6,6 @@ public class HomeWork {
 		int[] stick = { 5, 4, 4, 2, 2, 8};
 		// 최소값, 횟수 변수 선언
 		int min = 0;
-		int count = 0;
 		// 최소값 찾기
 		for(int i = 0; i < stick.length; i++) {
 			if(stick[i] < min || min == 0) {
@@ -14,11 +13,11 @@ public class HomeWork {
 			} // 각 막대기에서 짧은 막대기 만큼 자르기
 			for(int i = 0; i < stick.length; i++) {
 				stick[i] = stick[i] - min;
-				if(i >= 0)
-				
-				count++;
+				if(i >= 0
+						)
+
 				System.out.println(stick[i]);
 		}
-	System.out.println(count);	
+
 	}
 }
